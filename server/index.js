d48946131ce61193a964ddf12b66e43ba6a32002
Pablo <pablo.corso@veepee.com>
2021-01-11 18:13:15 -0300

    Add postgres database to heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 
 app.get("/", (req, res) => res.send("Hello from Heorku"));
 
