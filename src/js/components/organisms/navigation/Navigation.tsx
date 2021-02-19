c4353f82b0804e09d68a49cf20c1c942224c0894
Pablo Corso <pabcorso@gmail.com>
2021-02-19 17:55:45 +0100

    Hide create new link on not home pages

--- src/js/components/organisms/navigation/Navigation.tsx
+++ src/js/components/organisms/navigation/Navigation.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Paths } from "../../../config";
 import "./navigation.css";
 
 const Navigation = () => {
+  const location = useLocation();
+  const isOnHomePage = location.pathname === Paths.home;
   return (
     <nav className="nav">
       <Link to={Paths.home} className="nav-logo">
         elma-tcg
       </Link>
-      <Link to={Paths.newCard} className="nav-new-card">
-        Create new +
-      </Link>
+      {isOnHomePage && (
+        <Link to={Paths.newCard} className="nav-new-card">
+          Create new +
+        </Link>
+      )}
     </nav>
   );
 };
