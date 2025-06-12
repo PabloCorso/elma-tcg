d74b54ad5d5fab7d2cc9dce006bd88c2b9743e86
Pablo Corso <pabcorso@gmail.com>
2025-06-12 21:02:19 +0200

    Simplify data

--- app/routes/rules.tsx
+++ app/routes/rules.tsx
@@ -1,10 +1,14 @@
 import ReactMarkdown from "react-markdown";
 import rulesMarkdown from "#app/assets/rules.md?raw";
+import { TopBackLink } from "#app/components/top-back-link";
 
 export default function Rules() {
   return (
-    <main className="prose prose-invert mx-auto p-6">
-      <ReactMarkdown>{rulesMarkdown}</ReactMarkdown>
+    <main className="flex flex-col gap-6 p-4 pt-6">
+      <TopBackLink />
+      <div className="prose prose-invert mx-auto p-6">
+        <ReactMarkdown>{rulesMarkdown}</ReactMarkdown>
+      </div>
     </main>
   );
 }
